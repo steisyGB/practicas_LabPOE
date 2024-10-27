c95c4ec5f44cce76c2fd94ec9c1896557b49be1c
steisyGB <142046596+steisyGB@users.noreply.github.com>
2024-10-26 23:58:46 -0600

    Add files via upload

---
+++ BusinessLayer/Services/CarrerService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLayer;
+using CommonLayer.Entities;
+using DataAccessLayer;
+using DataAccessLayer.Repositories;
+
+namespace BusinessLayer.Services
+{
+    public class CarrerService
+    {
+        private CareerRepository _carrerRepository;
+
+        public CarrerService()
+        {
+            _carrerRepository = new CareerRepository();
+        }
+
+        public DataTable GetAllCarrers()
+        {
+            return _carrerRepository.GetCareers();
+        }
+
+        public void AddCareers(Career career)
+        {
+            _carrerRepository.AddCareer(career);
+        }
+
+        public void UpdateCareer(Career career)
+        {
+            _carrerRepository.EditCareer(career);
+        }
+
+        public void DeleteCareer(int id)
+        {
+            _carrerRepository.DeleteCareer(id);
+        }
+
+
+    }
+}
---
+++ BusinessLayer/Services/StudentService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLayer.Entities;
+using DataAccessLayer.DbConnection;
+using DataAccessLayer.Repositories;
+
+namespace BusinessLayer.Services
+{
+    public class StudentService
+    {
+        private StudentRepository _studentRepository;
+
+        public StudentService()
+        {
+            _studentRepository = new StudentRepository();
+        }
+
+        public DataTable GetAllStudent()
+        {
+            return _studentRepository.GetStudent();
+        }
+
+        public void AddStudent(Studen student)
+        {
+            _studentRepository.AddStudent(student);
+        }
+
+        public void UpdateStudent(Studen student)
+        {
+            _studentRepository.EditStudent(student);
+        }
+
+        public void DeleteCareer(int id)
+        {
+            _studentRepository.DeleteStudent(id);
+        }
+
+    }
+}
